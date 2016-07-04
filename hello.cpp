358eb1dfcc08ba7b33d256634838eddcb2b5e892
Dqiyue <2470782476@qq.com>
2016-07-04 20:12:40 +0800

    test

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: hello.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2016年07月04日 星期一 20时10分59秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"hello world"<<endl;
+    return 0;
+}
